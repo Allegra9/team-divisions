a4a93cb6f2fc5f1798502ec5758e4a00cad52591
Allegra9 <allegra.eng@gmail.com>
2022-01-11 18:26:58 +0200

    Solution

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,3 +1,21 @@
-export const getResults = (division: unknown, n: unknown) => {
-  // Your code here
-};
+interface Team {
+  name: string
+  points: number
+}
+
+const formatToString = (teams: Team[]): string =>
+  teams.map(team => team.name).join('\n')
+
+export const getResults = (division: Team[], n: number): string => {
+  if (division.length < 2 * n)
+    return `Error: division has only ${division.length} teams!`
+
+  const sorted = [...division].sort((a, b) => b.points - a.points)
+
+  const teamsToPromote = sorted.slice(0, n)
+  const teamsToRelegate = sorted.slice(-n)
+
+  return `Promote:\n${formatToString(
+    teamsToPromote
+  )}\n\nRelegate:\n${formatToString(teamsToRelegate)}`
+}
